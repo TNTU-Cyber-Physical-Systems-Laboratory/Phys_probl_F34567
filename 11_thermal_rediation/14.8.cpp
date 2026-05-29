c5a19de5ddc5567696ba5ee765bc51d6e8bb28a9
Andrew Matsur <andrew.matsur@gmail.com>
2026-05-29 18:55:21 +0300

    мені здається, що задача 14.8 має виглядати ось так

--- 11_thermal_rediation/14.8.cpp
+++ 11_thermal_rediation/14.8.cpp
@@ -4,30 +4,25 @@
 
 int main() {
     setlocale(LC_CTYPE, "ukr");
-    
-    //Константи
-     double sigma = 5.67e-8;
-     double R= 1.496e11;
-     const double S = 1.0; 
-     const double t = 1.0;
+
+    // Константи
+    double sigma = 5.67e-8; // Стала Стефана-Больцмана
+    double r_o = 1.496e11;  // Радіус земної орбіти (відстань від Землі до Сонця)
+    double r_s = 6.96e8;    // Радіус Сонця
 
     // Змінні
-    double T, r_sun;
-    
+    double T; // Температура поверхні Сонця
+
     std::cout << "~~~Визначення сонячної сталої~~~" << std::endl;
 
     std::cout << "Введіть температуру поверхні Сонця (К): ";
     std::cin >> T;
 
-    std::cout << "Введіть радіус Сонця (м): ";
-    std::cin >> r_sun;
+    // Розрахунок енергетичної світності Сонця
+    double R = sigma * std::pow(T, 4);
 
-    // Розрахунок енергетичної світності Сонця 
-    double W = sigma * std::pow(T, 4);
-
-    // Розрахунок сонячної сталої 
-    double C = W * std::pow(r_sun / R, 2);
-
+    // Розрахунок сонячної сталої
+    double C = R * std::pow(r_s / r_o, 2);
 
     // Виведення результатів
     std::cout << "\n~~~Результат~~~" << std::endl;
